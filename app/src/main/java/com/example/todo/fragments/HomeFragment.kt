f6a5061447b7a3fc342fa8f246af78430e76f2c4
vanhoan01 <nguyenvanhoan251201@gmail.com>
2023-03-09 10:34:00 +0700

    delete todo

--- app/src/main/java/com/example/todo/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/todo/fragments/HomeFragment.kt
@@ -5,24 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todo.R
 import com.example.todo.databinding.FragmentHomeBinding
 import com.example.todo.databinding.FragmentSignInBinding
+import com.example.todo.utils.ToDoAdapter
+import com.example.todo.utils.ToDoData
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.*
 
-class HomeFragment : Fragment(), AddTodoPopupFragment.DialogNextBtnClickListeners {
+class HomeFragment : Fragment(), AddTodoPopupFragment.DialogNextBtnClickListeners,
+    ToDoAdapter.ToDoAdapterClicksInterface {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var databaseRef: DatabaseReference
     private lateinit var navControl: NavController
     private lateinit var binding: FragmentHomeBinding
     private lateinit var popUpFragment: AddTodoPopupFragment
+    private lateinit var adapter: ToDoAdapter
+    private lateinit var mList: MutableList<ToDoData>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +41,7 @@ class HomeFragment : Fragment(), AddTodoPopupFragment.DialogNextBtnClickListener
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         init(view)
+        getDataFromFirebase()
         registerEvent()
     }
 
@@ -42,6 +49,35 @@ class HomeFragment : Fragment(), AddTodoPopupFragment.DialogNextBtnClickListener
         navControl = Navigation.findNavController(view)
         auth = FirebaseAuth.getInstance()
         databaseRef = FirebaseDatabase.getInstance().reference.child("Tasks").child(auth.currentUser?.uid.toString())
+
+        binding.mainRecyclerView.setHasFixedSize(true)
+        binding.mainRecyclerView.layoutManager = LinearLayoutManager(context)
+        mList = mutableListOf()
+        adapter = ToDoAdapter(mList)
+        adapter.setListener(this)
+        binding.mainRecyclerView.adapter = adapter
+    }
+
+    private fun getDataFromFirebase(){
+        databaseRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                mList.clear()
+                for(taskSnapShot in snapshot.children){
+                    val todoTask = taskSnapShot.key?.let{
+                        ToDoData(it, taskSnapShot.value.toString())
+                    }
+                    if(todoTask !=null){
+                        mList.add(todoTask)
+                    }
+                }
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     private fun registerEvent(){
@@ -65,5 +101,19 @@ class HomeFragment : Fragment(), AddTodoPopupFragment.DialogNextBtnClickListener
             }
             popUpFragment.dismiss()
         }
+    }
+
+    override fun onDeleteTaskBtnClicked(toDoData: ToDoData) {
+        databaseRef.child(toDoData.taskId).removeValue().addOnCompleteListener{
+            if (it.isSuccessful){
+                Toast.makeText(context, "Deleted successfully", Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(context, it.exception?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onEditTaskBtnClicked(toDoData: ToDoData) {
+        TODO("Not yet implemented")
     }
 }
